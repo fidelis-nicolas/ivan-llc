b64f9db1c4822ad80d0a85de75a73c88d74859e0
fidelis <fidelisnicolas@gmail.com>
2026-05-23 18:21:17 +0100

    Employee Management Service

--- src/main/java/com/ivanllc/ivanllc/controller/EmployeeServiceController.java
+++ src/main/java/com/ivanllc/ivanllc/controller/EmployeeServiceController.java
@@ -1,6 +1,7 @@
 package com.ivanllc.ivanllc.controller;
 
 import com.ivanllc.ivanllc.notification.Messages;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -10,14 +11,15 @@ import javafx.stage.Stage;
 
 import java.util.Objects;
 
-public class AdminDashboardController {
-    @FXML
-    Button btnEmployeeService;
+public class EmployeeServiceController {
 
-    public void openEmployeeService(){
+    @FXML
+    private Button btnDashboard;
+
+    public void openDashboard(ActionEvent actionEvent) {
         try {
-            Stage stage = (Stage) btnEmployeeService.getScene().getWindow();
-            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/ivanllc/ivanllc/employee-service-dashboard.fxml")));
+            Stage stage = (Stage) btnDashboard.getScene().getWindow();
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/ivanllc/ivanllc/admin-dashboard.fxml")));
             stage.setScene(new Scene(root, 900, 600));
             stage.setTitle("Employee Service ");
         } catch (Exception e) {
@@ -25,5 +27,4 @@ public class AdminDashboardController {
             Messages.error("Unable to open dashboard");
         }
     }
-
 }
